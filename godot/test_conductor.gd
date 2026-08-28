7703f80f8ec03b621e89356e1357f87d3cf4dde9
Manolo <manueldl12062003@gmail.com>
2026-08-29 00:21:44 +0200

    clean install

---
+++ godot/test_conductor.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@onready var conductor: Conductor = $Conductor
+@onready var label: Label = $Label
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	conductor.play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	label.text = "Beat: %.2f" % conductor.get_current_beat()
